05b762e195590693d52396732af1de6f18d0eadf
avi <avicon95@protonmail.com>
2022-11-01 20:02:31 -0500

    Readded /docs

---
+++ docs/src/immutable/chunks/1-697a48ec.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-4d4cacee.js";export{t as component};
--- docs/src/immutable/chunks/1-fcc0b63a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-d25ee11d.js";export{t as component};
