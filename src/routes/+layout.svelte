f680dfa75711edf0f15f71388e4b721e8b479581
avi <avicon95@protonmail.com>
2022-11-01 19:56:07 -0500

    Changes in svelte config adapter for github pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
     import "../app.css";
     import Header from "../lib/Header.svelte";
     import Footer from "../lib/Footer.svelte";
+    
+    export const prerender = true;
 </script>
   
 <Header />  
